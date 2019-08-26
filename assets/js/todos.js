c9669d1f2bd8b2ed09f241b0cfcca74a33dd471d
Chris Patrick <atomize101@gmail.com>
2019-08-26 11:26:34 -0400

    Adding in logic to click a todo

---
+++ assets/js/todos.js
@@ -0,0 +1,15 @@
+
+
+$('li').click(function() {
+	if($(this).css('color') === 'rgb(128, 128, 128)') {
+		$(this).css('color', 'black');
+		$(this).css('textDecoration', 'none');
+	}
+	else {
+			$(this).css({
+			color: 'gray',
+			textDecoration: 'line-through'	
+		});
+	}
+});
+
