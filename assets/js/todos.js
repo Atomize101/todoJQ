2e69a84238cce34a8ea7a02819377f05db83b0fe
Chris Patrick <atomize101@gmail.com>
2019-08-26 16:43:49 -0400

    Basic Functionality in place

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,15 +1,23 @@
 
 
-$('li').click(function() {
-	if($(this).css('color') === 'rgb(128, 128, 128)') {
-		$(this).css('color', 'black');
-		$(this).css('textDecoration', 'none');
-	}
-	else {
-			$(this).css({
-			color: 'gray',
-			textDecoration: 'line-through'	
-		});
-	}
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('completed');
 });
 
+$('ul').on('click','span', function(e) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	// stopPropagation is important since when clicking the X, it does not trigger another element click. 
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		// This pulls the value of the user input and stores it
+		var toDoTxt = $(this).val();
+		// This is appending a new list item after user hits enter. 
+		$('ul').append("<li><span>X</span> " + toDoTxt + "</li>");
+		$(this).val('');
+	}
+});
